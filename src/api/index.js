698b954df3448dc256fa015c820cfca9fa985a58
jianhong <1774643394@qq.com>
2018-04-09 11:03:06 +0800

    update

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-let host = 'http://localhost:3000';
 
+const host = 'http://localhost:3000';
 const login = (userName, password) => {
     return axios.post(host + '/api/user/login', {userName: userName, password: password})
 }
